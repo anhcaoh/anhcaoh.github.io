1b4a043b8b27a1a1f8e3ec4613183562b13b5aa3
Anh Cao <anhcaoh@gmail.com>
2014-05-30 19:42:34 -0400

    added rsvp form and gallery

--- javascripts/app.js
+++ javascripts/app.js
@@ -15,10 +15,10 @@ $(function(){
 		var currentScrollPosition = $(this).scrollTop();
 
 		if ( currentScrollPosition < initialScrollPosition ){
-			$("#menu li").show();
+			$("#menu").slideDown();
 		}
 		else {
-			$("#menu li").hide();
+			$("#menu").slideUp();
 		
 		}
 
@@ -27,11 +27,31 @@ $(function(){
 
 	$("img").on("click",function(){
 
-		// $("#container").css({"background-color":"red", "opacity":".2"});
 		$("img.expanded").not(this).removeClass("expanded").addClass("min");
 		$(this).toggleClass("expanded").toggleClass("min");
 
 		
 	});
 
+	$(".addr").on("click", function(){
+		$("#g-map").toggle();
+	})
+
+	$("#bring-guest").click(function(){
+		document.getElementById("ckb-guest").checked == true ? checkedIsYes() : checkedIsNo();
+
+		function checkedIsYes(){
+			$("#guest-confirm").empty().append("yes");
+			$(".plus1").show();
+		};
+
+		function checkedIsNo(){
+			
+			$("#guest-confirm").empty().append("no");
+			$(".plus1").hide();
+		};
+	
+	});
+
 });
+
