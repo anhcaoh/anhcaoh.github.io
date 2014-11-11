777fe8320a98aa3e572673c3d54656cda11e95d2
Anh Cao <anhcaoh@gmail.com>
2014-11-11 17:21:46 -0500

    UX miscs

--- scripts/init.js
+++ scripts/init.js
@@ -3,15 +3,24 @@ angular.module("meApp", [])
 
 .controller("ContentController", ["$scope",function($scope){
 
-  $scope.headlines = [
+  $scope.allAboutMe = [
 
-    {"nameId":"Hi-im-Anh", "imgSrc": "https://media.licdn.com/mpr/mpr/shrink_200_200/p/3/000/20f/0d6/1eba86d.jpg","content":"Hi, I'm Anh"},
+    {"nameId":"hi-im-Anh", "imgSrc": "https://media.licdn.com/mpr/mpr/shrink_200_200/p/3/000/20f/0d6/1eba86d.jpg","content":"Hi, I'm Anh"},
 
-    {"nameId":"i-am-vietnamese","content":"I am Vietnamese."},
+    {"nameId":"i-am-vietnamese","content":"I flew from here Vietnam to USA nine years ago."},
 
     {"nameId":"i-speak", "content":"I speak JavaScript, HTML, and CSS."},
 
-    {"nameId":"i-am-a", "content":"I am a designer, creator, and dancer."}
+    {"nameId":"i-breathe", "content":"I breathe User Experience."},
+
+    {"nameId":"i-dance", "content":"When I'm not designing, am playing soccer and (break)dancing."}
+
   ];
+
+  $scope.contactMe = {
+
+    "email":"AnhCaoH@gmail.com",
+    "phone":"(678) 602-1591"
+  }
 
 }]);
