ec050d1eefc95309ffd4ca64fde40210ca13e5aa
Anh Cao <anhcaoh@gmail.com>
2014-11-11 17:28:18 -0500

    Updated font style
    22pt

--- scripts/init.js
+++ scripts/init.js
@@ -7,7 +7,7 @@ angular.module("meApp", [])
 
     {"nameId":"hi-im-Anh", "imgSrc": "https://media.licdn.com/mpr/mpr/shrink_200_200/p/3/000/20f/0d6/1eba86d.jpg","content":"Hi, I'm Anh"},
 
-    {"nameId":"i-am-vietnamese","content":"I flew from here Vietnam to USA nine years ago."},
+    {"nameId":"i-am-vietnamese","content":"I flew from Vietnam to USA nine years ago."},
 
     {"nameId":"i-speak", "content":"I speak JavaScript, HTML, and CSS."},
 
