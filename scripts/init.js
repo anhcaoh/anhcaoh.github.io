d7d67b76da523c07950c8c6b64592cc55473294f
Anh Cao <anhcaoh@gmail.com>
2014-11-11 20:09:53 -0500

    Added bg colors

--- scripts/init.js
+++ scripts/init.js
@@ -5,15 +5,15 @@ angular.module("meApp", [])
 
   $scope.allAboutMe = [
 
-    {"nameId":"hi-im-Anh", "imgSrc": "https://media.licdn.com/mpr/mpr/shrink_200_200/p/3/000/20f/0d6/1eba86d.jpg","content":"Hi, I'm Anh"},
+    {"nameId":"hi-im-Anh", "imgSrc": "https://media.licdn.com/mpr/mpr/shrink_200_200/p/3/000/20f/0d6/1eba86d.jpg","content":"Hi, I'm Anh", "bgColor":""},
 
-    {"nameId":"i-am-vietnamese","content":"I flew from Vietnam to USA nine years ago."},
+    {"nameId":"i-am-vietnamese","content":"I flew from Vietnam to the USA nine years ago.", "bgColor":"#FCF09E"},
 
-    {"nameId":"i-speak", "content":"I speak JavaScript, HTML, and CSS."},
+    {"nameId":"i-speak", "content":"I speak JavaScript, HTML, and CSS.", "bgColor":""},
 
-    {"nameId":"i-breathe", "content":"I breathe User Experience."},
+    {"nameId":"i-breathe", "content":"I breathe User Experience.", "bgColor":"lightblue"},
 
-    {"nameId":"i-dance", "content":"When I'm not designing, am playing soccer and (break)dancing."}
+    {"nameId":"i-dance", "content":"When I'm not designing, am playing soccer and (break)dancing.", "bgColor":"#000","color":"#fff"}
 
   ];
 
